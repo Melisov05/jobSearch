54c5336a0727f8528f225822a1a54691f6293691
Melisov01 <aytemirmelisov@gmail.com>
2024-03-08 16:05:07 +0600

    created a model for resumes

---
+++ src/main/java/com/example/demo/model/Resumes.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+import lombok.Builder;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Builder
+@Data
+public class Resumes {
+    private int applicantId;
+    private String name;
+    private int categoryId;
+    private double salary;
+    private boolean isActive;
+    private LocalDate createdDate;
+    private LocalDate updatedDate;
+}
