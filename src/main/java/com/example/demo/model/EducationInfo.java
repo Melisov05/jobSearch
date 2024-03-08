9ab10d32eb1bcd50114074647bf3b5c6d9679079
Melisov01 <aytemirmelisov@gmail.com>
2024-03-08 16:07:32 +0600

    created a model for education Info

---
+++ src/main/java/com/example/demo/model/EducationInfo.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+public class EducationInfo {
+    private int resumeId;
+    private String institution;
+    private String program;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String degree;
+}
