dd8c0d6a3aab615021fe2a6893b95ff9f6272451
Melisov01 <aytemirmelisov@gmail.com>
2024-03-26 19:21:38 +0600

    created methods for work Experience and EducationInfoDao

---
+++ src/main/java/com/example/demo/dao/EducationInfoDao.java
@@ -0,0 +1,28 @@
+package com.example.demo.dao;
+
+import com.example.demo.model.EducationInfo;
+import lombok.AllArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class EducationInfoDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    public void createEducationInfo(EducationInfo educationInfo) {
+        String sql = "insert into EDUCATION_INFO(RESUME_ID, INSTITUTION, PROGRAM, START_DATE, END_DATE, DEGREE) " +
+                "VALUES (?, ?, ?, ?, ?, ?)";
+        jdbcTemplate.update(sql, educationInfo.getResumeId(), educationInfo.getInstitution(),
+                educationInfo.getProgram(), educationInfo.getStartDate(),
+                educationInfo.getEndDate(), educationInfo.getDegree());
+    }
+
+    public void updateEducationInfo(EducationInfo educationInfo) {
+        String sql = "update EDUCATION_INFO set INSTITUTION = ?, PROGRAM = ?, START_DATE = ?, " +
+                "END_DATE = ?, DEGREE = ? WHERE ID = ?";
+        jdbcTemplate.update(sql, educationInfo.getInstitution(), educationInfo.getProgram(),
+                educationInfo.getStartDate(), educationInfo.getEndDate(),
+                educationInfo.getDegree(), educationInfo.getId());
+    }
+}
--- src/main/java/com/example/demo/dao/WorkExperienceDao.java
+++ src/main/java/com/example/demo/dao/WorkExperienceDao.java
@@ -27,4 +27,4 @@ public class WorkExperienceDao {
                 workExperienceInfo.getId());
     }
 }
-}
+
